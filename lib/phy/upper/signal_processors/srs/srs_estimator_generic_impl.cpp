1ae32d89fa1af6ce42890e7b34a6824561d0dc32
Sadia Afrose <95015966+sadiamimi@users.noreply.github.com>
2026-04-28 13:58:27 -0400

    Change SRS_CSI_COLLECTION_MODE from 4 to 0

--- lib/phy/upper/signal_processors/srs/srs_estimator_generic_impl.cpp
+++ lib/phy/upper/signal_processors/srs/srs_estimator_generic_impl.cpp
@@ -102,7 +102,7 @@ using namespace srsran;
 //   - Rotation: New file created when current file reaches 100MB
 //   - Use case: Complete channel characterization with SNR analysis
 //
-#define SRS_CSI_COLLECTION_MODE 4
+#define SRS_CSI_COLLECTION_MODE 0
 
 // File size limit (100 MB) - creates new file when reached
 #define SRS_CSI_MAX_FILE_SIZE (100UL * 1024 * 1024)
